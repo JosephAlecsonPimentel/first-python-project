e21e7269da9987e8965f77eba39c6e3b07bd5615
Joseph.Pimentel <jaspimentel31@gmail.com>
2024-10-09 15:24:59 +0800

    Lab6

---
+++ Pimentel_Act 1/act 1.py
@@ -0,0 +1,23 @@
+ # Enter your average Grades if greater than 85
+print(float(input("Enter the Average Grades:")))    
+input("Qualifies for Academic Honors")    
+firstname = "Joseph"
+lastname = "Pimentel"
+print("Name: " +firstname  + " " + lastname)
+print("Average Grades: 85")
+print("Student is in Honorary List?: YES")
+
+# Enter your average Grades if greater than 95
+print(float(input("Enter the Average Grades:")))
+input("Qualifies for Highest Honors")
+print("Name: " +firstname  + " " + lastname)
+print("Average Grades: 95")
+print("Student is in Honorary List?: YES")
+
+# Enter your average Grades if less than 85
+print(float(input("Enter the Average Grades:")))
+input("Not Qualifies for Honors")
+print("Name: " +firstname  + " " + lastname)
+print("Average Grades: 84")
+print("Student is in Honorary List?: No Better Luck Next Time")
+
